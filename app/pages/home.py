7ed96145cee3c5e07369ca3cfe9f7921b3822366
Umair539 <umohammed235@outlook.com>
2025-12-04 11:52:33 +0000

    added metrics for each feature on dashboard

--- app/pages/home.py
+++ app/pages/home.py
@@ -15,6 +15,13 @@ st.markdown(
     """
 )
 
+plasma = pd.read_csv("data/raw/plasma.csv")
+
+st.markdown(
+    f"Data last updated at {plasma['time_tag'].iloc[-1]}",
+    unsafe_allow_html=True,
+)
+
 solar = pd.read_csv("data/transformed/solar_agg.csv")
 solar.loc[:, "time"] = pd.to_datetime(solar["time"])
 
@@ -24,7 +31,6 @@ dst.loc[:, "time"] = pd.to_datetime(dst["time"])
 kp = pd.read_csv("data/transformed/kp.csv")
 kp.loc[:, "time"] = pd.to_datetime(kp["time"])
 
-plasma = pd.read_csv("data/raw/plasma.csv")
 
 col1, col2, col3 = st.columns(3)
 
@@ -34,6 +40,8 @@ temp_delta = solar["temperature_mean"].iloc[-2] / solar["temperature_mean"].iloc
 pressure_delta = solar["pressure_mean"].iloc[-2] / solar["pressure_mean"].iloc[-3]
 bz_delta = solar["bz_mean"].iloc[-2] / solar["bz_mean"].iloc[-3]
 bt_delta = solar["bt_mean"].iloc[-2] / solar["bt_mean"].iloc[-3]
+dst_delta = dst["dst"].iloc[-1] / dst["dst"].iloc[-2]
+kp_delta = kp["Kp"].iloc[-1] / kp["Kp"].iloc[-2]
 
 with col1:
     st.metric(
@@ -77,5 +85,23 @@ with col3:
         label="IMF Bt",
         value=f'{solar["bt_mean"].iloc[-2]} nT',
         delta=(f"{round(bt_delta * 100 - 100, 2)}%"),
+        border=True,
+    )
+
+c1, c2 = st.columns(2)
+
+with c1:
+    st.metric(
+        label="Dst Index",
+        value=f'{dst["dst"].iloc[-1]} nT',
+        delta=(f"{round(dst_delta * 100 - 100, 2)}%"),
+        border=True,
+    )
+
+with c2:
+    st.metric(
+        label="Kp Index",
+        value=f'{kp["Kp"].iloc[-1]} nT',
+        delta=(f"{round(kp_delta * 100 - 100, 2)}%"),
         border=True,
     )
