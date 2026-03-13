2393062b3d29bb20ae50bf1f59c5b04a2552543d
Umair539 <umohammed235@outlook.com>
2026-03-13 20:32:21 +0000

    solar wind hourly graphs with sql is working

--- app/pages/solar_wind.py
+++ app/pages/solar_wind.py
@@ -1,16 +1,12 @@
 import streamlit as st
-import pandas as pd
+import altair as alt
 from streamlit_autorefresh import st_autorefresh
+from datetime import datetime
 
 st_autorefresh(30000)
+conn = st.session_state.noaa_data_db
 
 st.title("Real Time Solar Wind Properties 🛰️")
-
-solar = pd.read_csv("data/transformed/solar.csv")
-solar.loc[:, "time"] = pd.to_datetime(solar["time"])
-
-solar_agg = pd.read_csv("data/transformed/solar_agg.csv")
-solar_agg.loc[:, "time"] = pd.to_datetime(solar_agg["time"])
 
 col1, col2, col3 = st.columns(3)
 
@@ -19,7 +15,7 @@ with col1:
         label="Select time resolution", options=["Minutely", "Hourly"], index=0
     )
 
-columns = solar.columns.str.title()[1:]
+columns = ["Speed", "Density", "Temperature", "Pressure", "Bz", "Bt"]
 
 with col2:
     features = st.multiselect(
@@ -29,31 +25,20 @@ with col2:
 with col3:
     aggregation = st.selectbox(
         label="Select aggregation method",
-        options=["Mean", "Standard deviation", "Interquartile range"],
+        options=["Mean", "Standard deviation"],
         disabled=(resolution == "Minutely"),
     )
 
-if resolution == "Hourly":
-    suffix = {
-        "Mean": "_mean",
-        "Standard deviation": "_std",
-        "Interquartile range": "_iqr",
-    }
-    filtered_cols = [
-        col for col in solar_agg.columns if col.endswith(suffix[aggregation])
-    ]
-    df = solar_agg[["time"] + filtered_cols]
-    rename_mapping = {
-        col: col.replace(suffix[aggregation], "") for col in filtered_cols
-    }
-    df = df.rename(columns=rename_mapping)
-else:
-    df = solar
-
 win = {"Minutely": 24 * 60, "Hourly": 24}
 
+if resolution == "Hourly":
+    query = f"SELECT COUNT(DISTINCT strftime('%Y-%m-%d %H', time)) AS unique_hour_count FROM solar;"
+    total_rows = conn.query(query, ttl=60).squeeze()
+else:
+    total_rows = conn.query("SELECT COUNT(*) FROM solar", ttl=60).squeeze()
+
 min_val = 0
-max_val = len(df) - win[resolution] + (-1 if resolution == "Hourly" else 0)
+max_val = total_rows - win[resolution] - (1 if resolution == "Hourly" else 0)
 
 s = st.slider(
     " ",
@@ -64,28 +49,52 @@ s = st.slider(
     label_visibility="hidden",
 )
 
+if resolution == "Hourly":
+    cols_to_query = []
+    for col in features:
+        cols_to_query.append(f", avg({col}) AS {col.lower()}")
+    cols_to_query = "".join(cols_to_query)
+
+    data_query = (
+        f"SELECT "
+        f"strftime('%Y-%m-%d %H:00:00', time) AS hourly_bucket"
+        f"{cols_to_query}"
+        f" FROM solar "
+        f"GROUP BY hourly_bucket "
+        f"ORDER BY hourly_bucket ASC "
+        f"LIMIT {win[resolution]} "
+        f"OFFSET {s};"
+    )
+    x_axis = "hourly_bucket"
+else:
+    cols_to_query = ", ".join(["time"] + features)
+    data_query = (
+        f"SELECT {cols_to_query} FROM solar LIMIT {win[resolution]} OFFSET {s};"
+    )
+    x_axis = "time"
+
 c1, c2 = st.columns(2)
 
-with c1:
-    st.markdown(
-        (
-            f"<div style='text-align: left;'>"
-            f"Displaying data from {df['time'][s]} "
-            f"to {df['time'][s + win[resolution] - 1]}"
-            f"</div>"
-        ),
-        unsafe_allow_html=True,
-    )
+# with c1:
+#     st.markdown(
+#         (
+#             f"<div style='text-align: left;'>"
+#             f"Displaying data from {df['time'][s]} "
+#             f"to {df['time'][s + win[resolution] - 1]}"
+#             f"</div>"
+#         ),
+#         unsafe_allow_html=True,
+#     )
 
-with c2:
-    st.markdown(
-        (
-            f"<div style='text-align: right;'>"
-            f"Data last updated at {solar['time'].iloc[-1]}"
-            f"</div>"
-        ),
-        unsafe_allow_html=True,
-    )
+# with c2:
+#     st.markdown(
+#         (
+#             f"<div style='text-align: right;'>"
+#             f"Data last updated at {solar['time'].iloc[-1]}"
+#             f"</div>"
+#         ),
+#         unsafe_allow_html=True,
+#     )
 
 label = {
     "Density": "Particle density (p/cm3)",
@@ -108,8 +117,8 @@ for feature in features:
     )
 
     st.line_chart(
-        data=df.iloc[s : s + win[resolution]],
-        x="time",
+        data=conn.query(data_query, ttl=60),
+        x=x_axis,
         y=str(feature.lower()),
         x_label="Time",
         y_label=label[str(feature)],
