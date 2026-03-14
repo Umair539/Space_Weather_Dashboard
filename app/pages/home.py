1fbb5c2714f720c2ad32d74381358ed2785c4e5e
Umair539 <umohammed235@outlook.com>
2026-03-14 21:12:55 +0000

    completed transition to sql at frontend instead of pandas

--- app/pages/home.py
+++ app/pages/home.py
@@ -1,9 +1,10 @@
 import streamlit as st
-import pandas as pd
 from streamlit_autorefresh import st_autorefresh
 import plotly.graph_objects as go
+from run_app import data_last_synced
+import altair as alt
 
-st_autorefresh(60000)
+conn = st.session_state.noaa_data_db
 
 st.title("Space Weather Dashboard 🪐")
 
@@ -16,52 +17,57 @@ st.markdown(
     """
 )
 
-plasma = pd.read_csv("data/raw/plasma.csv")
-
 st.markdown(
-    f"Data last updated at {plasma['time_tag'].iloc[-1]}",
+    f"<div style='text-align: right; font-style: italic; color: gray;'>"
+    f"Data last synced at {data_last_synced()}"
+    f"</div>",
     unsafe_allow_html=True,
 )
 
-solar = pd.read_csv("data/transformed/solar.csv")
-solar.loc[:, "time"] = pd.to_datetime(solar["time"])
+solar = conn.query("SELECT * FROM solar ORDER BY time DESC LIMIT 61")
+dst = conn.query("SELECT * FROM dst ORDER BY time DESC LIMIT 24")
+kp = conn.query("SELECT * FROM kp ORDER BY time DESC LIMIT 1")
 
-dst = pd.read_csv("data/transformed/dst.csv")
-dst.loc[:, "time"] = pd.to_datetime(dst["time"])
 
-kp = pd.read_csv("data/transformed/kp.csv")
-kp.loc[:, "time"] = pd.to_datetime(kp["time"])
-
-speed_delta = solar["speed"].iloc[-1] / solar["speed"].iloc[-2]
-density_delta = solar["density"].iloc[-1] / solar["density"].iloc[-2]
-temp_delta = solar["temperature"].iloc[-1] / solar["temperature"].iloc[-2]
-pressure_delta = solar["pressure"].iloc[-1] / solar["pressure"].iloc[-2]
-bz_delta = solar["bz"].iloc[-1] / solar["bz"].iloc[-2]
-bt_delta = solar["bt"].iloc[-1] / solar["bt"].iloc[-2]
-dst_delta = dst["dst"].iloc[-1] / dst["dst"].iloc[-2]
-kp_delta = kp["Kp"].iloc[-1] / kp["Kp"].iloc[-2]
+#
+speed_delta = solar["speed"].iloc[0] / solar["speed"].iloc[60]
+density_delta = solar["density"].iloc[0] / solar["density"].iloc[60]
+temp_delta = solar["temperature"].iloc[0] / solar["temperature"].iloc[60]
+pressure_delta = solar["pressure"].iloc[0] / solar["pressure"].iloc[60]
+bz_delta = solar["bz"].iloc[0] / solar["bz"].iloc[60]
+bt_delta = solar["bt"].iloc[0] / solar["bt"].iloc[60]
 
 c1, c2 = st.columns((0.5, 0.5))
 
 with c1:
     st.markdown("")
-    st.markdown("")
     st.markdown(
-        f'<div style="font-size: 24px; text-align: center;">Dst Index: {dst["dst"].iloc[-1]} nT</div>',
+        f'<div style="font-size: 24px; text-align: center; margin-top: 20px;">'
+        f'Dst Index: {dst["dst"].iloc[0]} nT</div>',
         unsafe_allow_html=True,
     )
-    st.line_chart(
-        data=dst.iloc[-24:],
-        x="time",
-        y="dst",
-        x_label="Time",
-        y_label="Dst (nT)",
-        color="#ff0000",
+
+    dst_chart = (
+        alt.Chart(dst)
+        .mark_line(color="#ff0000")
+        .encode(
+            x=alt.X(
+                "time:T",
+                axis=alt.Axis(
+                    labelAngle=0,
+                    tickCount=6,
+                    format="%H:%M",
+                    title="Time",
+                ),
+            ),
+            y=alt.Y("dst:Q", title="Dst (nT)"),
+        )
+        .properties(height=350)
     )
 
+    st.altair_chart(dst_chart, use_container_width=True)
 
 with c2:
-
     step_colours = [
         {"range": [0, 5.0], "color": "green"},
         {"range": [5.0, 7.0], "color": "orange"},
@@ -71,14 +77,9 @@ with c2:
     fig = go.Figure(
         go.Indicator(
             mode="gauge+number",
-            value=kp["Kp"].iloc[-1],
-            number={
-                "prefix": "Kp: ",
-            },
-            title={
-                "text": "Kp Index",
-                "font": {"size": 24},
-            },
+            value=kp["Kp"].iloc[0],
+            number={"prefix": "Kp: "},
+            title={"text": "Kp Index", "font": {"size": 24}},
             gauge={
                 "shape": "angular",
                 "axis": {"range": [0, 9]},
@@ -86,32 +87,28 @@ with c2:
                 "bar": {"color": "rgba(0, 0, 0, 0)"},
                 "threshold": {
                     "line": {"color": "black", "width": 3},
-                    "value": kp["Kp"].iloc[-1],
+                    "value": kp["Kp"].iloc[0],
                     "thickness": 1,
                 },
             },
         )
     )
-
-    fig.update_layout(
-        height=400,
-    )
-
-    st.plotly_chart(fig, width="stretch")
+    fig.update_layout(height=400)
+    st.plotly_chart(fig, use_container_width=True)
 
 col1, col2, col3 = st.columns(3)
 
+# Metrics
 with col1:
     st.metric(
         label="Solar Wind Speed",
-        value=f'{solar["speed"].iloc[-1]} km/s',
+        value=f'{solar["speed"].iloc[0]} km/s',
         delta=(f"{round(speed_delta * 100 - 100, 2)}%"),
         border=True,
     )
-
     st.metric(
         label="Solar Wind Density",
-        value=f'{solar["density"].iloc[-1]} p/cm3',
+        value=f'{solar["density"].iloc[0]} p/cm3',
         delta=(f"{round(density_delta * 100 - 100, 2)}%"),
         border=True,
     )
@@ -119,14 +116,13 @@ with col1:
 with col2:
     st.metric(
         label="Solar Wind Temperature",
-        value=f'{solar["temperature"].iloc[-1]} K',
+        value=f'{solar["temperature"].iloc[0]} K',
         delta=(f"{round(temp_delta * 100 - 100, 2)}%"),
         border=True,
     )
-
     st.metric(
         label="Solar Wind Pressure",
-        value=f'{solar["pressure"].iloc[-1]} nPa',
+        value=f'{solar["pressure"].iloc[0]} nPa',
         delta=(f"{round(pressure_delta * 100 - 100, 2)}%"),
         border=True,
     )
@@ -134,14 +130,15 @@ with col2:
 with col3:
     st.metric(
         label="IMF Bz",
-        value=f'{solar["bz"].iloc[-1]} nT',
+        value=f'{solar["bz"].iloc[0]} nT',
         delta=(f"{round(bz_delta * 100 - 100, 2)}%"),
         border=True,
     )
-
     st.metric(
         label="IMF Bt",
-        value=f'{solar["bt"].iloc[-1]} nT',
+        value=f'{solar["bt"].iloc[0]} nT',
         delta=(f"{round(bt_delta * 100 - 100, 2)}%"),
         border=True,
     )
+
+st_autorefresh(60000)
