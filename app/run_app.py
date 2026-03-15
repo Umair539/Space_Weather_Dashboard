9d50cacef63dfccdab6aa3e00489839d29126651
Umair539 <umohammed235@outlook.com>
2026-03-15 15:49:29 +0000

    fix module import bug

--- app/run_app.py
+++ app/run_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 import threading
+import sys
+from pathlib import Path
+
+current_dir = Path(__file__).resolve().parent
+project_root = current_dir.parent
+if str(project_root) not in sys.path:
+    sys.path.insert(0, str(project_root))
+
 from scripts.run_etl import run_etl_pipeline
 
 
