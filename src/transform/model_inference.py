535d321a68ab6cede057b76f6a3e22fd8fefda05
Umair539 <umohammed235@outlook.com>
2026-04-06 01:04:13 +0100

    updated requirements.txt and split model_inference.py into sub functions

--- src/transform/model_inference.py
+++ src/transform/model_inference.py
@@ -1,8 +1,19 @@
 from keras import utils, models
 import pandas as pd
 
+dst_model = models.load_model("model/model.keras")
 
-def model_inference(model_inputs, dst):
+
+def model_inference(model_inputs, dst, model=dst_model):
+    model_inputs = prepare_dataset(model_inputs)
+    predictions = run_inference(model_inputs, model)
+    predictions = denormalise_predictions(predictions)
+    dst = align_predictions(predictions, dst)
+
+    return dst
+
+
+def prepare_dataset(model_inputs):
     model_inputs = utils.timeseries_dataset_from_array(
         data=model_inputs,
         targets=None,
@@ -11,17 +22,24 @@ def model_inference(model_inputs, dst):
         sequence_stride=1,
         shuffle=False,
     )
+    return model_inputs
 
-    model = models.load_model("model/model.keras")
+
+def run_inference(model_inputs, model):
     predictions = model.predict(model_inputs).flatten()
+    return predictions
 
+
+def denormalise_predictions(predictions):
     stats = (-8, 19)
     predictions = (predictions * stats[1]) + stats[0]
+    return predictions
 
+
+def align_predictions(predictions, dst):
     dst = dst[-len(predictions) :].copy()
     dst["predictions"] = predictions
     next_hour = dst.index[-1] + pd.Timedelta(hours=1)
     dst.loc[next_hour, :] = None
     dst["predictions"] = dst["predictions"].shift(1)
-
     return dst
